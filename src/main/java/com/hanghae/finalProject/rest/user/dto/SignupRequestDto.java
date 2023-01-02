27acd7264a3a5195ae696484927047133046bb3d
yundukhyun <kiml2175@nate.com>
2023-01-02 23:45:52 +0900

    Refactor : 회원가입 email 어노테이션 추가(#1)
    로그인 기능 업데이트 예정

--- src/main/java/com/hanghae/finalProject/rest/user/dto/SignupRequestDto.java
+++ src/main/java/com/hanghae/finalProject/rest/user/dto/SignupRequestDto.java
@@ -3,6 +3,7 @@ package com.hanghae.finalProject.rest.user.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -11,7 +12,6 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 public class SignupRequestDto {
 
-//    @Pattern (regexp="^(?=.*?[0-9])(?=.*?[a-z]).{5,10}$")
     @Size(min = 5, max = 10)
     private String username;
 
@@ -19,6 +19,7 @@ public class SignupRequestDto {
     @Pattern (regexp="^.(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$")
     private String password;
 
+    @Email
     @NotBlank
     private String email;
 
