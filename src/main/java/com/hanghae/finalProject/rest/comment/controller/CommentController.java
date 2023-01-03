5c6c246a65916f38991265c0bffbb281cdb7b617
deokbin <imdorothy3@gmail.com>
2023-01-03 14:38:56 +0900

    chore: swagger사용을 위한 어노테이션 추가

--- src/main/java/com/hanghae/finalProject/rest/comment/controller/CommentController.java
+++ src/main/java/com/hanghae/finalProject/rest/comment/controller/CommentController.java
@@ -5,26 +5,32 @@ import com.hanghae.finalProject.config.dto.ResponseDto;
 import com.hanghae.finalProject.config.errorcode.Code;
 import com.hanghae.finalProject.rest.comment.dto.CommentRequestDto;
 import com.hanghae.finalProject.rest.comment.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "comment", description = "댓글 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/meetings/{meetingId}")
 public class CommentController {
     private final CommentService commentService;
 
+    @Operation(summary = "댓글 조회")
     @GetMapping("/comments")
     public ResponseDto getCommentList(@PathVariable Long postId) {
         return DataResponseDto.of(commentService.getCommentList(postId));
     }
 
+    @Operation(summary = "댓글 작성")
     @PostMapping("/comments")
     public ResponseDto createComment(@PathVariable Long postId,
                                      @RequestBody CommentRequestDto commentRequestDto) {
         return DataResponseDto.of(commentService.createComment(postId, commentRequestDto), Code.CREATE_COMMENT.getStatusMsg());
     }
 
+    @Operation(summary = "댓글 삭제")
     @DeleteMapping("/comments/{commentId}")
     public ResponseDto deleteComment(@PathVariable Long commentId) {
         commentService.deleteComment(commentId);
