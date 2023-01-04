3a746bfb9a38bfcf60e3355a1d94a715c2c1ee6c
Youngju Jang <joj1043@nate.com>
2023-01-04 13:21:27 +0900

    chore: swagger 설명 추가 (#13)

--- src/main/java/com/hanghae/finalProject/rest/meeting/controller/MeetingController.java
+++ src/main/java/com/hanghae/finalProject/rest/meeting/controller/MeetingController.java
@@ -7,6 +7,7 @@ import com.hanghae.finalProject.rest.meeting.dto.MeetingLinkRequestDto;
 import com.hanghae.finalProject.rest.meeting.dto.MeetingRequestDto;
 import com.hanghae.finalProject.rest.meeting.dto.MeetingUpdateRequestDto;
 import com.hanghae.finalProject.rest.meeting.service.MeetingService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
@@ -17,29 +18,49 @@ import javax.validation.Valid;
 public class MeetingController {
 
     private final MeetingService  meetingService;
-
+    
+    @ApiOperation (value = "모임 상세 조회")
     @GetMapping("/meetings/{id}")
     public ResponseDto getMeeting(@PathVariable Long id) {
         return DataResponseDto.of(meetingService.getMeeting(id));
     }
-
+    
+    @ApiOperation (value = "모임 전체 조회")
+    @GetMapping("/meetings")
+    public ResponseDto getMeetings(
+         @RequestParam(value="sortby", defaultValue = "popular") int sortBy,
+         @RequestParam(value="category", required = false) String category,
+         @RequestParam(value="meetingId", required = false, defaultValue = "5") int mee
+    ) {
+        // 1. 인기순, 카테고리 없는버전 - 우선이거 (+ 무한스크롤)
+            // 1.1 카테고리 있는버전
+        // 2. 신규순, 카테고리 없는버전
+            // 2.1 카테고리 있는버전
+        // 무한스크롤 적용필요
+        return DataResponseDto.of(meetingService.getMeetings(sortBy, category));
+    }
+    
+    @ApiOperation (value = "모임 생성")
     @PostMapping("/meetings")
     public ResponseDto createMeeting(@RequestBody @Valid MeetingRequestDto requestDto) {
         return DataResponseDto.of(meetingService.createMeeting(requestDto), Code.CREATE_MEETING.getStatusMsg());
     }
-
+    
+    @ApiOperation (value = "모임 수정")
     @PatchMapping("/meetings/{id}")
     public ResponseDto updateAllMeeting(@PathVariable Long id, @RequestBody @Valid MeetingUpdateRequestDto requestDto) {
         meetingService.updateAllMeeting(id,requestDto);
         return ResponseDto.of(true, Code.UPDATE_MEETING);
     }
-
+    
+    @ApiOperation (value = "모임 링크 수정")
     @PatchMapping("/meetings/{id}/link")
     public ResponseDto updateLink(@PathVariable Long id, @RequestBody @Valid MeetingLinkRequestDto requestDto) {
         meetingService.updateLink(id, requestDto);
         return ResponseDto.of(true, Code.UPDATE_LINK);
     }
-
+    
+    @ApiOperation (value = "모임 삭제")
     @DeleteMapping("/meetings/{id}")
     public ResponseDto deleteMeeting(@PathVariable Long id) {
         meetingService.deleteMeeting(id);
