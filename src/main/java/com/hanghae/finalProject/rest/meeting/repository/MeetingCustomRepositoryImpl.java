b65d448fd22d14277df563e2253cc0c1c3fbd626
Youngju Jang <joj1043@nate.com>
2023-01-04 22:04:08 +0900

    feat : 무한스크롤 적용 (#23)

--- src/main/java/com/hanghae/finalProject/rest/meeting/repository/MeetingCustomRepositoryImpl.java
+++ src/main/java/com/hanghae/finalProject/rest/meeting/repository/MeetingCustomRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.hanghae.finalProject.rest.meeting.repository;
 
 import com.hanghae.finalProject.rest.meeting.model.Meeting;
 import com.hanghae.finalProject.rest.meeting.model.QMeeting;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
@@ -22,7 +23,18 @@ public class MeetingCustomRepositoryImpl implements MeetingCustomRepository{
      public List<Meeting> findAllByOrderByIdDesc(Long meetingIdx) {
           return jpaQueryFactory
                .selectFrom(meeting)
+               .where(
+                    ltBookId(meetingIdx)
+               )
                .orderBy(meeting.id.desc())
+               .limit(5)
                .fetch();
+     }
+     
+     private BooleanExpression ltBookId(Long meetingIdx) {
+          if (meetingIdx == null) {
+               return null; // BooleanExpression 자리에 null이 반환되면 조건문에서 자동으로 제거된다
+          }
+          return meeting.id.lt(meetingIdx);
      }
 }
