99c08ed5d429ed73f373181125e9530c0afda161
deokbin <imdorothy3@gmail.com>
2023-01-05 18:16:25 +0900

    chore: 안쓰는 import 정리(#27)

--- src/main/java/com/hanghae/finalProject/rest/attendant/controller/AttendantController.java
+++ src/main/java/com/hanghae/finalProject/rest/attendant/controller/AttendantController.java
@@ -2,12 +2,10 @@ package com.hanghae.finalProject.rest.attendant.controller;
 
 import com.hanghae.finalProject.config.dto.DataResponseDto;
 import com.hanghae.finalProject.config.dto.ResponseDto;
-import com.hanghae.finalProject.config.errorcode.Code;
 import com.hanghae.finalProject.rest.attendant.service.AttendantService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "attendant", description = "모임 참석 API")
