73471e26ced4d125774f15c1f3876418e27ab85d
Youngju Jang <joj1043@nate.com>
2023-01-06 20:24:53 +0900

    chore : cicd용(#14)

--- src/main/java/com/hanghae/finalProject/rest/user/controller/UserController.java
+++ src/main/java/com/hanghae/finalProject/rest/user/controller/UserController.java
@@ -53,7 +53,6 @@ public class UserController {
 ////          return DataResponseDto.of("data test");
 //          // 3. DATA X, MSG 따로
 //          return ResponseDto.of(true, Code.USER_SIGNUP_SUCCESS);
-//     }
     
     //https://kauth.kakao.com/oauth/authorize?client_id=ced49bfdb65f5f152e2e43f12e88bd86&redirect_uri=https://sparta-hippo.shop/api/user/kakao/callback&response_type=code
     //https://kauth.kakao.com/oauth/authorize?client_id=ced49bfdb65f5f152e2e43f12e88bd86&redirect_uri=http://localhost:8080/api/user/kakao/callback&response_type=code
