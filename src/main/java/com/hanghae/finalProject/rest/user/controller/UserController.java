3f73e79ec2b2ac8283ff49f49699f217721dc3d5
Youngju Jang <joj1043@nate.com>
2023-01-06 23:28:50 +0900

    Fix : 유저컨트롤러 api: user > users 로 변경 (#1, #2, #6)

--- src/main/java/com/hanghae/finalProject/rest/user/controller/UserController.java
+++ src/main/java/com/hanghae/finalProject/rest/user/controller/UserController.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 
 @Tag (name="user", description = "사용자 API")
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     
@@ -48,7 +48,7 @@ public class UserController {
     }
 
 //          // 1. data o , msg o
-////          return DataResponseDto.of("data test", "test 성공"); 
+////          return DataResponseDto.of("data test", "test 성공");
 //          // 2. data o msg 정상
 ////          return DataResponseDto.of("data test");
 //          // 3. DATA X, MSG 따로
