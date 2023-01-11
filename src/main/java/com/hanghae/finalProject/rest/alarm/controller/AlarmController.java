fd4767e34fdee75fc036d8778d969c659bdefbf0
rgngr <wlals8369@naver.com>
2023-01-11 23:47:14 +0900

    feat: alarm 기능 구현 ing...(#50)

--- src/main/java/com/hanghae/finalProject/rest/alarm/controller/AlarmController.java
+++ src/main/java/com/hanghae/finalProject/rest/alarm/controller/AlarmController.java
@@ -1,25 +1,54 @@
 package com.hanghae.finalProject.rest.alarm.controller;
 
-import com.hanghae.finalProject.rest.alarm.service.AlarmService;
+import com.hanghae.finalProject.config.errorcode.Code;
+import com.hanghae.finalProject.config.exception.RestApiException;
+import com.hanghae.finalProject.config.jwt.JwtUtil;
+import com.hanghae.finalProject.config.util.SecurityUtil;
+import com.hanghae.finalProject.rest.user.model.User;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class AlarmController {
 
-    private final AlarmService alarmService;
+    public static Map<Long, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
+    private final JwtUtil jwtUtil;
 
     @ApiOperation(value = "알람")
-    @GetMapping(value = "/alarm", produces = "text/event-stream")
-    public SseEmitter alarm(@RequestHeader(value = "Last-Alarm-ID", required = false, defaultValue = "") String lastAlarmId) {
-        return alarmService.alarm(lastAlarmId);
+    @GetMapping(value = "/sub", consumes = MediaType.ALL_VALUE)
+    public SseEmitter subscribe(@RequestParam String token) {
+
+        User user = SecurityUtil.getCurrentUser();
+        if (user == null) throw new RestApiException(Code.NOT_FOUND_AUTHORIZATION_IN_SECURITY_CONTEXT);
+
+        Long userId = user.getId();
+
+        SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
+        try {
+            sseEmitter.send(SseEmitter.event().name("connect"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sseEmitters.put(userId, sseEmitter);
+
+        sseEmitter.onCompletion(() -> sseEmitters.remove(userId));
+        sseEmitter.onTimeout(() -> sseEmitters.remove(userId));
+        sseEmitter.onError((e) -> sseEmitters.remove(userId));
+
+        return sseEmitter;
     }
+
 
 }
