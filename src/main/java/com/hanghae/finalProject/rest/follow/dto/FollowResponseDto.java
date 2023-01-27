115a1e406f8a172cc6d9b963b1d25f50c148e57a
Youngju Jang <joj1043@nate.com>
2023-01-27 22:03:20 +0900

    Fix : 팔로우리스트 responseDto 필드명변경, 애노테이션추가 (#56)

--- src/main/java/com/hanghae/finalProject/rest/follow/dto/FollowResponseDto.java
+++ src/main/java/com/hanghae/finalProject/rest/follow/dto/FollowResponseDto.java
@@ -1,6 +1,5 @@
 package com.hanghae.finalProject.rest.follow.dto;
 
-import com.hanghae.finalProject.rest.follow.model.Follow;
 import com.hanghae.finalProject.rest.user.model.User;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,12 +11,12 @@ public class FollowResponseDto {
 
     private String username;
 
-    private String profile;
+    private String profileUrl;
 
     public FollowResponseDto(User user){
         this.id = user.getId();
         this.username = user.getUsername();
-        this.profile = user.getProfileUrl();
+        this.profileUrl = user.getProfileUrl();
 
 
     }
