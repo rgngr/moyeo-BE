2d50609c3cf5e3341c02d43ad4e726d02430720f
Youngju Jang <joj1043@nate.com>
2023-01-30 15:03:43 +0900

    Fix : 프로필이미지 변경시 파일명 난수화되어 들어가도록 수정(#79) - 기존파일명으로 s3업로드시 파일명 같을경우 덮어씌워짐

--- src/main/java/com/hanghae/finalProject/config/S3/S3Uploader.java
+++ src/main/java/com/hanghae/finalProject/config/S3/S3Uploader.java
@@ -8,14 +8,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor    // final 멤버변수가 있으면 생성자 항목에 포함시킴
@@ -36,12 +39,24 @@ public class S3Uploader {
      }
 
      private String upload(File uploadFile, String dirName) {
-          String fileName = dirName + "/" + uploadFile.getName();
+          String fileName = dirName + "/" + createFileName(uploadFile.getName());
           String uploadImageUrl = putS3(uploadFile, fileName);
 
           removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
 
           return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
+     }
+     
+     private String createFileName(String fileName) { // 먼저 파일 업로드 시, 파일명을 난수화하기 위해 random으로 돌립니다.
+          return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+     }
+     
+     private String getFileExtension(String fileName) { // file 형식이 잘못된 경우를 확인하기 위해 만들어진 로직이며, 파일 타입과 상관없이 업로드할 수 있게 하기 위해 .의 존재 유무만 판단하였습니다.
+          try {
+               return fileName.substring(fileName.lastIndexOf("."));
+          } catch (StringIndexOutOfBoundsException e) {
+               throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일(" + fileName + ") 입니다.");
+          }
      }
 
      private String putS3(File uploadFile, String fileName) {
