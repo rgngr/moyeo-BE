a9ac8bce39f153d56fc98eab3d6a89aad1c3c5d1
Youngju Jang <joj1043@nate.com>
2023-02-03 00:24:55 +0900

    Feat :  meeting controller e2e 테스트중 (#129) - 모임생성시 파일도 같이보내는 케이스 - multipart/form 으로 보내는 거임

--- src/test/java/com/hanghae/finalProject/rest/meeting/controller/MeetingControllerTest.java
+++ src/test/java/com/hanghae/finalProject/rest/meeting/controller/MeetingControllerTest.java
@@ -2,6 +2,7 @@ package com.hanghae.finalProject.rest.meeting.controller;
 
 import com.hanghae.finalProject.AcceptanceTest;
 import io.restassured.RestAssured;
+import io.restassured.builder.MultiPartSpecBuilder;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -109,11 +111,32 @@ class MeetingControllerTest extends AcceptanceTest {
           // 로그인 토큰구하기
           String accessToken = getToken();
           // Given
-          Map<String, Object> params = new HashMap<>();
-          
           // When
-          
+          ExtractableResponse<Response> response =
+               RestAssured
+                    .given().log().all()
+                    .header("Authorization", accessToken)
+                    .multiPart(new MultiPartSpecBuilder(new File("./src/main/resources/image/rian1.jpg")).controlName("image").fileName("rian1.jpg").with().charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("테스트다아아").controlName("title").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("일단모여").controlName("category").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("2023-06-10").controlName("startDate").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("20:00:00").controlName("startTime").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("3").controlName("duration").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("피카피카").controlName("content").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("10").controlName("maxNum").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("ZOOM").controlName("platform").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("").controlName("link").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("false").controlName("secret").charset("utf-8").build())
+                    .multiPart(new MultiPartSpecBuilder("").controlName("password").charset("utf-8").build())
+                    .contentType("multipart/form-data")
+                    .when()
+                    .post("/api/meetings")
+                    .then().log().all()
+                    .extract();
           // Then
+          assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+          assertThat(response.body().jsonPath().getString("data.title")).isEqualTo("테스트다아아");
+          assertThat(response.body().jsonPath().getString("statusMsg")).isEqualTo("모임 글 작성 성공");
           // .multiPart("data",body,"application/json")
           // .multiPart("file[0]", new File(file1),"multipart/form-data")
           
