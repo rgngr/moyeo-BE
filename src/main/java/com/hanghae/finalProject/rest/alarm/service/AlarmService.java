a0cf35a5b421ff2789f92b161fa1373f3de5f61b
rgngr <wlals8369@naver.com>
2023-02-09 00:48:17 +0900

    Fix : searchMeetings 에 deletedIsFalse 조건 추가 (#50)

--- src/main/java/com/hanghae/finalProject/rest/alarm/service/AlarmService.java
+++ src/main/java/com/hanghae/finalProject/rest/alarm/service/AlarmService.java
@@ -360,7 +360,9 @@ public class AlarmService {
 
         // 각 모임 30분 전 알림으로
         for (Meeting meeting : meetings) {
-            alarmBefore30(meeting);
+            if (!meeting.isDeleted()) {
+                alarmBefore30(meeting);
+            }
         }
 //        for (MeetingAlarmListDto meetingAlarmListDto : meetingAlarmListDtos) {
 //            alarmBefore30(meetingAlarmListDto);
